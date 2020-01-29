adb5a9327996fbf47308b9e6551b1111bdc2363d
Fernando Giovanini <fernando.giovanini@coliquio.de>
2020-01-29 11:49:22 +0100

    Enable server set origin

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const logger = new Logger('bootstrap');
 
+  const serverConfig = config.get('server');
+
   const app = await NestFactory.create(AppModule);
 
   if (process.env.NODE_ENV === 'development') {
     app.enableCors();
+  } else {
+    app.enableCors({
+      origin: serverConfig.origin,
+    });
+    logger.log(`Accepting requets from origin ${serverConfig.origin}`);
   }
 
-  const serverConfig = config.get('server');
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
 
